0b618bf1f45f7a115543a9abf7f9c0b8267c724a
hasabirr <222011282@stis.ac.id>
2025-01-06 15:00:52 +0700

    fix credentials

--- deployment/deployment.R
+++ deployment/deployment.R
@@ -1,7 +1,5 @@
 library(rsconnect)
-rsconnect::setAccountInfo(name='rafifhasabi',
-                          token='6043895F410298D2F75C3980497DFCB1',
-                          secret='h0Qu4wepbgPQL37/cGoQtPHAd4Cbh2VcTYjU1x51')
-rsconnect::deployApp("E:/OneDrive/Work/Training/BPS Orientation/PDRB/gdp-dashboard")
+rsconnect::setAccountInfo(name='', token='', secret='')
+rsconnect::deployApp("")
 
 
